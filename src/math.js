4847926e869b526690691c5cf167efcddb44edee
Athus <athuslopes23@gmai.com>
2025-04-05 12:02:06 -0300

    Aula02 implementando funções matemáticas.

--- src/math.js
+++ src/math.js
@@ -9,15 +9,24 @@
  */
 function somaDobroNumerosParesFuncional(numeros){
     // TODO: Insira seu código funcional aqui
+    return numeros
+        .filter(num => num % 2 === 0)
+        .map(num => num * 2)
+        .reduce((soma, num) => soma + num, 0);
 }
-
 /**
  * Realiza a soma do dobro do valor de cada número par em um vetor de números.
  * A função deve ser implementada utilizando a abordagem estruturada (procedural) convencional.
  * @param {Array} numeros vetor com os números desejados
  */
 function somaDobroNumerosParesProcedural(numeros){
-    // TODO: Insira seu código estruturado aqui   
-}
-
+    // TODO: Insira seu código estruturado aqui
+    let soma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {
+            soma += numeros[i] * 2;
+        }
+    }
+    return soma;
+    }
 export { somaDobroNumerosParesFuncional, somaDobroNumerosParesProcedural };
